a48d43a45a2e78eb3e68f45ebea9b4a9a4435d01
Tim Caswell <tim@creationix.com>
2010-01-01 14:46:56 -0600

    Convert the test script into an automated unit test.

--- test/test.js
+++ test/test.js
@@ -1,28 +1,37 @@
-// Requires nodeJS 0.1.13 or greater to run <http://nodejs.org/>
-// just type `node test.js` on the command line
-
-// Load the haml-js library from the current directory
-var Haml = require("./haml");
 var file = require('file');
-var sys = require('sys');
+var posix = require('posix');
+var assert = require('assert');
+process.mixin(require('sys'));
 
-// Set up a scope for our view to render in
-var scope = {
-  print_date: function () {
-    return (new Date()).toDateString();
-  },
-  current_user: {
-    address: "Richardson, TX",
-    email: "tim@creationix.com",
-    bio: "Experienced software professional..."
-  }
-};
+var Haml = require("../lib/haml");
 
-// Load, parse, and render the html.  The result it passed to the callback puts
-// which prints it to the terminal.
-file.read('test.haml').addCallback(function (source) {
-  sys.puts(Haml.render(source, {
-    locals: scope
-  }));
+posix.readdir('.').addCallback(function (files) {
+  files.forEach(function (haml_file) {
+    var m = haml_file.match(/^(.*)\.haml/),
+        base;
+    if (!m) {
+      return;
+    }
+    base = m[1];
+
+    function load_haml(scope) {
+      file.read(haml_file).addCallback(function (haml) {
+        file.read(base + ".html").addCallback(function (expected) {
+          var actual = Haml.render(haml, scope);
+          assert.equal(actual, expected);
+        });
+      });
+    }
+
+    // Load scope
+    if (files.indexOf(base + ".js") >= 0) {
+      file.read(base + ".js").addCallback(function (js) {
+        load_haml(eval("(" + js + ")"));
+      });
+    } else {
+      load_haml({});
+    }
+  });
 });
+
 
