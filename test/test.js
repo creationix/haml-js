80bf3503e128649da5a672dc5ae74051177e1524
Tim Caswell <tim@creationix.com>
2010-03-31 18:26:31 -0500

    Run all tests and stop on first failure.

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ var Haml = require("../lib/haml");
 
 fs.readdir('.', function (err, files) {
   files.forEach(function (haml_file) {
-    if (haml_file !== 'standard.haml') return;
+    Haml.debug = haml_file === '.haml';
     var m = haml_file.match(/^(.*)\.haml/),
         base;
     if (!m) {
@@ -19,19 +19,20 @@ fs.readdir('.', function (err, files) {
       fs.readFile(haml_file, function (err, haml) {
         fs.readFile(base + ".html", function (err, expected) {
           try {
-            var js = Haml.compile(haml);
-            var fn = new Function('locals', js);
-            puts(fn+"");
+            var fn = Haml(haml);
+            var js = fn+"";
             var actual = fn.call(scope.context, scope.locals);
             assert.equal(actual, expected);
             puts(haml_file + " Passed")
           } catch (e) {
             var message = e.name;
             if (e.message) message += ": " + e.message;
+            puts(haml_file + " FAILED")
             puts(e.stack || message);
             puts("\nJS:\n\n" + js);
             puts("\nActual:\n\n" + actual);
             puts("\nExpected:\n\n" + expected);
+            process.exit();
           }
         });
       });
